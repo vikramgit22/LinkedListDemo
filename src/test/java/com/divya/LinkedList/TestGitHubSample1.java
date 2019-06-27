1190720bba40219037d2eb3da629eef0ddb79861
TestRepo <vikramkrishna7333@gmail.com>
2019-06-27 11:26:34 +0530

    finished for 1st release

---
+++ src/test/java/com/divya/LinkedList/TestGitHubSample1.java
@@ -0,0 +1,15 @@
+package com.divya.LinkedList;
+
+public class TestGitHubSample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a=10;
+		int b=20;
+		float c = a+b;
+		System.out.println(c);	
+	}
+
+}
